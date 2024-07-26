741620873ce00344f5c8c87cc54512103c9c712a
Iwan BK <iwanbk@gmail.com>
2024-07-26 10:47:28 +0700

    support env variables

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # zsocks
+
+## Usage
+
+as env vars
+```bash
+LISTEN_ADDR=127.0.0.1:9800 USERNAME=admin PASSWORD=nimda ./target/debug/zsocks
+```
+or 
+```bash
+./target/debug/zsocks --listen-addr=127.0.0.1:9800 --password=admin --username=nimda
+```
+
+more logging
+```bash
+RUST_LOG=info LISTEN_ADDR=127.0.0.1:9800 USERNAME=admin PASSWORD=nimda ./target/debug/zsocks
+```
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,58 @@
 extern crate log;
 
+use clap::Parser;
 use fast_socks5::{
     server::{Config, SimpleUserPassword, Socks5Server, Socks5Socket},
-    Result, SocksError,
+    Result,
 };
-use log::{error, info, warn};
+use log::{error, info};
 use std::future::Future;
-use structopt::StructOpt;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::task;
 use tokio_stream::StreamExt;
 
-/// # How to use it:
-///
-/// Listen on a local address, authentication-free:
-///     `$ RUST_LOG=debug cargo run -- --listen-addr 127.0.0.1:1337 no-auth`
-///
-/// Listen on a local address, with basic username/password requirement:
-///     `$ RUST_LOG=debug cargo run -- --listen-addr 127.0.0.1:1337 password --username admin --password password`
-///
-#[derive(Debug, StructOpt)]
-#[structopt(
-    name = "socks5-server",
-    about = "A simple implementation of a socks5-server."
-)]
+// socks5 proxy server
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 struct Opt {
-    /// Bind on address address. eg. `127.0.0.1:1080`
-    #[structopt(short, long)]
+    /// Bind on address address. eg. `127.0.0.1:9800`
+    #[arg(short, long, env = "LISTEN_ADDR", default_value = "127.0.0.1:9800")]
     pub listen_addr: String,
 
     /// Request timeout
-    #[structopt(short = "t", long, default_value = "10")]
+    #[arg(short, long, env = "REQUEST_TIMEOUT", default_value = "10")]
     pub request_timeout: u64,
 
-    /// Choose authentication type
-    #[structopt(subcommand, name = "auth")] // Note that we mark a field as a subcommand
-    pub auth: AuthMode,
+    /// Username
+    #[arg(long, env = "USERNAME", required = true)]
+    pub username: String,
+
+    /// Password
+    #[arg(long, env = "PASSWORD", required = true)]
+    pub password: String,
 
     /// Don't perform the auth handshake, send directly the command request
-    #[structopt(short = "k", long)]
+    #[arg(short, long, default_value_t = false)]
     pub skip_auth: bool,
-}
-
-/// Choose the authentication type
-#[derive(StructOpt, Debug)]
-enum AuthMode {
-    NoAuth,
-    Password {
-        #[structopt(short, long)]
-        username: String,
-
-        #[structopt(short, long)]
-        password: String,
-    },
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let opt: Opt = Opt::parse();
     env_logger::init();
 
-    spawn_socks_server().await
+    spawn_socks_server(opt).await
 }
 
-async fn spawn_socks_server() -> Result<()> {
-    let opt: Opt = Opt::from_args();
-    let mut config = Config::default();
+async fn spawn_socks_server(opt: Opt) -> Result<()> {
+    let mut config: Config<SimpleUserPassword> = Config::default();
     config.set_request_timeout(opt.request_timeout);
     config.set_skip_auth(opt.skip_auth);
     config.set_dns_resolve(true);
-
-    let config = match opt.auth {
-        AuthMode::NoAuth => {
-            warn!("No authentication has been set!");
-            config
-        }
-        AuthMode::Password { username, password } => {
-            if opt.skip_auth {
-                return Err(SocksError::ArgumentInputError(
-                    "Can't use skip-auth flag and authentication altogether.",
-                ));
-            }
-
-            info!("Simple auth system has been set.");
-            config.with_authentication(SimpleUserPassword { username, password })
-        }
-    };
+    let config = config.with_authentication(SimpleUserPassword {
+        username: opt.username,
+        password: opt.password,
+    });
 
     let listener = <Socks5Server>::bind(&opt.listen_addr).await?;
     let listener = listener.with_config(config);
